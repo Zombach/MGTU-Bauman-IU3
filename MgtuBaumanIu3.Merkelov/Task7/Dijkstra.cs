bbbdb7f5b7d5011f447f1bc78e3e8d450beb4d2c
ZLoo <Kozlov.s.v.1992@gmail.com>
2023-10-06 02:22:53 +0300

    Update

---
+++ MgtuBaumanIu3.Merkelov/Task7/Dijkstra.cs
@@ -0,0 +1,62 @@
+﻿namespace MgtuBaumanIu3.Merkelov.Task7;
+
+public class Dijkstra
+{
+    private int MinimumDistance(int[] distance, bool[] shortestPathTreeSet, int verticesCount)
+    {
+        int min = int.MaxValue;
+        int minIndex = 0;
+
+        for (int i = 0; i < verticesCount; i++)
+        {
+            if (shortestPathTreeSet[i] || distance[i] > min) { continue; }
+            min = distance[i];
+            minIndex = i;
+        }
+
+        return minIndex;
+    }
+
+    private void Print(int[] distance, int verticesCount)
+    {
+        Console.WriteLine("Вершина\tРасстояние от источника");
+
+        for (int i = 0; i < verticesCount; ++i)
+        {
+            Console.WriteLine("{0}\t  {1}", i, distance[i]);
+        }
+    }
+
+    public void DijkstraAlgo(int[,] graph, int source, int verticesCount)
+    {
+        int[] distance = new int[verticesCount];
+        bool[] shortestPathTreeSet = new bool[verticesCount];
+
+        for (int i = 0; i < verticesCount; i++)
+        {
+            distance[i] = int.MaxValue;
+            shortestPathTreeSet[i] = false;
+        }
+
+        distance[source] = 0;
+
+        for (int i = 0; i < verticesCount - 1; i++)
+        {
+            int u = MinimumDistance(distance, shortestPathTreeSet, verticesCount);
+            shortestPathTreeSet[u] = true;
+
+            for (int j = 0; j < verticesCount; ++j)
+            {
+                if
+                (
+                    !shortestPathTreeSet[j] &&
+                    Convert.ToBoolean(graph[u, j]) &&
+                    distance[u] != int.MaxValue &&
+                    distance[u] + graph[u, j] < distance[j]
+                )
+                { distance[j] = distance[u] + graph[u, j]; }
+            }
+        }
+        Print(distance, verticesCount);
+    }
+}
--- MgtuBaumanIu3.Merkelov/Task7/Quest.cs
+++ MgtuBaumanIu3.Merkelov/Task7/Quest.cs
@@ -9,73 +9,24 @@ namespace MgtuBaumanIu3.Merkelov.Task7;
 /// </summary>
 public class Quest : BaseQuest
 {
+    private readonly int[,] _graph =
+    {
+        {0, 7, 0, 0, 0, 0, 0, 0, 0, 0},
+        {7, 0, 4, 2, 4, 0, 0, 0, 0, 0},
+        {0, 4, 0, 1, 0, 0, 0, 0, 0, 0},
+        {0, 2, 1, 0, 0, 0, 0, 0, 0, 0},
+        {0, 4, 0, 0, 0, 2, 2, 0, 0, 0},
+        {0, 0, 0, 0, 2, 0, 2, 12, 0, 0},
+        {0, 0, 0, 0, 2, 2, 0, 3, 0, 0},
+        {0, 0, 0, 0, 0, 12, 3, 0, 2, 1},
+        {0, 0, 0, 0, 0, 0, 0, 2, 0, 1},
+        {0, 0, 0, 0, 0, 0, 0, 1, 1, 0}
+    };
     public override void Start()
     {
-        throw new NotImplementedException("Не реализовано!");
-        Dictionary<char, Dictionary<char, int>> adjacencyList = new()
-        {
-            {'A', new Dictionary<char, int> { {'B', 7 } } },
-            {'B', new Dictionary<char, int> { {'A', 7 } } },
-            {'C', new Dictionary<char, int> { {'B', 4 }, {'D', 1 } } },
-            {'D', new Dictionary<char, int> { {'B', 2 }, {'C', 1 } } },
-            {'E', new Dictionary<char, int> { {'B', 4 }, {'F', 2 }, {'G', 2 } } },
-            {'F', new Dictionary<char, int> { {'E', 2 }, {'G', 2 }, {'A', 12 } } },
-            {'G', new Dictionary<char, int> { {'E', 2 }, {'F', 2 }, {'H', 3 } } },
-            {'H', new Dictionary<char, int> { {'G', 3 }, {'I', 2 }, {'J', 1 } } },
-            {'I', new Dictionary<char, int> { {'H', 2 }, {'J', 1 } } },
-            {'J', new Dictionary<char, int> { {'H', 1 }, {'J', 1 } } }
-        }; 
-        char startNode = 'B';
-        Dictionary<char, int?> distances = Dijkstra(adjacencyList, startNode);
+        Dijkstra.DijkstraAlgo(_graph, 1, 10);
+        Console.ReadKey();
+        //throw new NotImplementedException("Не реализовано!");
 
-        foreach (var node in distances)
-        {
-            Console.WriteLine($"Shortest distance from {startNode} to {node.Key}: {node.Value}");
-        }
-    }
-    public Dictionary<char, int?> Dijkstra(Dictionary<char, Dictionary<char, int>> graph, char startNode)
-    {
-        Dictionary<char, int?> distances = new()
-        {
-            [startNode] = 0
-        };
-
-        HashSet<char> visited = new();
-
-        while (visited.Count < graph.Count)
-        {
-            char? currentNode = GetMinDistanceNode(distances, visited);
-            if(currentNode is null) { continue; }
-
-            visited.Add((char)currentNode);
-
-            foreach (var neighbor in graph[(char)currentNode])
-            {
-                if(distances[(char)currentNode] is null) { continue; }
-                int distance = (int)distances[(char)currentNode] + neighbor.Value;
-                if (distance < distances[neighbor.Key])
-                {
-                    distances[neighbor.Key] = distance;
-                }
-            }
-        }
-
-        return distances;
-    }
-
-    public char? GetMinDistanceNode(Dictionary<char, int?> distances, HashSet<char> visited)
-    {
-        char? minNode = null;
-
-        foreach (var node in distances.Keys)
-        {
-            if (!visited.Contains(node) && distances[node] is not null)
-            {
-                int? minDistance = distances[node];
-                minNode = node;
-            }
-        }
-
-        return minNode;
     }
 }
